217f53b8b3d4972d78113813dcae6883ed1506c0
liubin <752791962@qq.com>
2017-11-15 17:24:05 +0800

    1234

---
+++ laravel_form/app/Http/Controllers/StudentController.php
@@ -0,0 +1,13 @@
+<?php
+    namespace App\Http\Controllers;
+    /**
+     *
+     */
+    class StudentController extends Controller
+    {
+
+      public function index()
+      {
+        return view('student.index');
+      }
+    }
